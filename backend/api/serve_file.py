ec494e9854c3b29e63665a091bb48e47877cda63
kevin <kmathew@peloton-technologies.com>
2024-10-10 16:49:50 -0700

    fixing worker timeout with background tasks

--- backend/api/serve_file.py
+++ backend/api/serve_file.py
@@ -9,16 +9,22 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+
 # Endpoint to serve the Excel file
-@router.get("/download/{filename}", response_class=FileResponse)
+@router.get("/file", response_class=FileResponse)
 async def download_file(filename: str):
     """
     Download the generated Excel file.
     Args:
     - filename: The name of the Excel file to download.
     """
-    file_path = os.path.join("files", filename)
+    file_path = os.path.join(os.getcwd(), "files", filename)
+    print(f"[INFO] Downloading file: {file_path}")
     if not os.path.isfile(file_path):
         raise HTTPException(status_code=404, detail="File not found.")
-    
-    return FileResponse(file_path, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', filename=filename)
+
+    return FileResponse(
+        file_path,
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        filename=filename,
+    )
--- backend/utils/settings.py
+++ backend/utils/settings.py
@@ -2,3 +2,8 @@
 from countries.cambodia import get_communes_geodataframe  # Import from cambodia.py
 
 # This file should now only include global settings or configurations.
+# Define allowed origins for CORS
+origins = [
+    "http://localhost:3000",  # For local testing
+    "https://agritech-prod.vercel.app",  # Deployed frontend on Vercel
+]
